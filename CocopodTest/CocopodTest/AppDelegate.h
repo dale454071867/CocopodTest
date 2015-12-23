5e068e48aff19edbbbaef18b32f9e35e3b36169a
zhoujie <454071867@qq.com>
2015-12-23 14:04:44 +0800

    no message

---
+++ CocopodTest/CocopodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocopodTest
+//
+//  Created by 周杰 on 15/12/23.
+//  Copyright © 2015年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocopodTest/CocopodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocopodTest
+//
+//  Created by 周杰 on 15/12/23.
+//  Copyright © 2015年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
