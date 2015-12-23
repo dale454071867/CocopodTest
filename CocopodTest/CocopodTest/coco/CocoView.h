5bfe76a2afe8dc2d8199bd8d6b2c4a3ad49d7086
zhoujie <454071867@qq.com>
2015-12-23 14:43:07 +0800

    no message

---
+++ CocopodTest/CocopodTest/coco/CocoView.h
@@ -0,0 +1,13 @@
+//
+//  CocoView.h
+//  CocopodTest
+//
+//  Created by 周杰 on 15/12/23.
+//  Copyright © 2015年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CocoView : UIView
+
+@end
---
+++ CocopodTest/CocopodTest/coco/CocoView.m
@@ -0,0 +1,21 @@
+//
+//  CocoView.m
+//  CocopodTest
+//
+//  Created by 周杰 on 15/12/23.
+//  Copyright © 2015年 周杰. All rights reserved.
+//
+
+#import "CocoView.h"
+
+@implementation CocoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
